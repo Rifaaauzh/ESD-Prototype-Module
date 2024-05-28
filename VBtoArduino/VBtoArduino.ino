4537ab2bd3c8fbb6517f3349d3104b7c643b5108
Rf <101931924+Rifaaauzh@users.noreply.github.com>
2024-05-28 14:21:15 +0800

    Update VBtoArduino.ino

--- VBtoArduino/VBtoArduino.ino
+++ VBtoArduino/VBtoArduino.ino
@@ -10,12 +10,6 @@ void loop() {
  delay(100);
  float voltage = (sensorValue * 3.3) / 1023.0;
  delay(100);
-  /*if (Serial.available() > 0) {
-    String data = Serial.readString();
-    delay(100);
-    Serial.print("Received: ");
-    Serial.println(data);
-  }*/
-  Serial.println(voltage);
-  delay(2000);
+ Serial.println(voltage);
+ delay(2000);
 }
